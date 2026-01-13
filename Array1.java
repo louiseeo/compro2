52e793984f62f6da56fbacdc397267d9efdbf594
louiseeo <louisenicole.madriaga@lorma.edu>
2026-01-14 03:58:26 +0800

    Added

---
+++ Array1.java
@@ -0,0 +1,8 @@
+public class Array1{
+    public static void main(String args[]){
+        int[] arr = {1, 25, 7, 11, 9, 18, 12, 14, 15, 23};
+        for (int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
