69b58c39038998120365b024890c328b958a75d5
louiseeo <louisenicole.madriaga@lorma.edu>
2026-01-14 04:30:42 +0800

    Added Array1.java

--- Array1.java
+++ Array1.java
@@ -1,19 +1,18 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Array1 {
     public static void main(String args[]) {
-        int[] arr = { 1, 25, 7, 11, 9, 18, 12, 14, 15, 23 };
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        int[] array = { 1, 25, 7, 11, 9, 18, 12, 14, 15, 23 };
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
         }
 
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int num = input.nextInt();
         int index = -1;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == num) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == num) {
                 index = i;
                 break;
             }
