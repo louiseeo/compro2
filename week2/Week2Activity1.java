93ee69b0e7e0d3dfa8b0accb3e78b7ba9828e6f1
louiseeo <louisenicole.madriaga@lorma.edu>
2026-01-20 04:11:44 +0800

    new

---
+++ week2/Week2Activity1.java
@@ -0,0 +1,21 @@
+package week2;
+
+public class Week2Activity1 {
+    public static void main(String[] args) {
+        int[] theaterRow = {0, 0, 0, 0, 0, 0, 0, 0};
+        theaterRow[2] = 1;
+
+        int seatAvailableCounter = 0;
+        for (int i = 0; i < theaterRow.length; i++){
+            if (theaterRow[i] == 0) {
+                System.out.println("Available");
+                seatAvailableCounter++;
+            }
+            else
+                System.out.println("Not Available");
+        }
+        System.out.println("The total available seat is " + seatAvailableCounter);
+        
+    }
+    
+}
