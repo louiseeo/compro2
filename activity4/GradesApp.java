dbf4d5222c00f3999964e640106c36428545fbe7
louiseeo <louisenicole.madriaga@lorma.edu>
2026-02-11 11:11:48 +0800

    update

--- activity4/GradesApp.java
+++ activity4/GradesApp.java
@@ -5,6 +5,8 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class GradesApp {
+    // For counting of subjects
+    static int count = 0; 
     // Arrays to store subject names and its grades
     static String[] subjectName;
     static double[][] gradeData;
@@ -16,7 +18,7 @@ public class GradesApp {
 
         Scanner sc = new Scanner(System.in);
 
-        for (int r = 0; r < 5; r++) {
+        while (true) {
             // Display menu
             System.out.println("""
                     Menu
@@ -24,47 +26,91 @@ public class GradesApp {
                     [2] Exit
                     """);
 
-            System.out.print("Choice: ");
-            int choice = sc.nextInt();
+            int c = 0;
+            // Add validation for choice
+            while (true) {
+                System.out.print("Choice: ");
+                try {
+                    c = sc.nextInt();
+                    if (c == 1 || c == 2) {
+                        break; // exit loop if valid input
+                    } else {
+                        System.out.println("Invalid choice. Enter 1 or 2.\n");
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid choice. Enter 1 or 2.\n");
+                    sc.nextLine();
+                }
+            }
 
-            if (choice == 1) {
+            if (c == 1) {
+                // Stops if max subjects is entered
+                if (count == 50) {
+                    System.out.println("Maximum number of subjects is reached.");
+                    break;
+                }
+
                 // Prompt user to enter subject
                 System.out.print("\nEnter subject: ");
-                subjectName[r] = sc.next();
+                subjectName[count] = sc.next();
 
-                // Prompt user to enter its grades per subject
-                System.out.print("Prelim: ");
-                try {
-                    gradeData[r][0] = sc.nextDouble();
-                } catch (InputMismatchException e) {
-                    System.out.println("Invalid input. Numbers from 1 to 100 only.");
-                    sc.nextLine(); // clear bad input
+                // Prompt user to enter prelim grade
+                double p = 0;
+                while (true) {
+                    System.out.print("Prelim: ");
+                    try {
+                        p = sc.nextDouble();
+                        if (p >= 1 && p <= 100) {
+                            gradeData[count][0] = p;
+                            break; // escape if user entered a valid input
+                        } else
+                            System.out.println("Invalid input. Enter from 1 to 100 only.\n");
+                    } catch (InputMismatchException e) {
+                        System.out.println("Invalid input. Enter from 1 to 100 only.\n");
+                        sc.nextLine();
+                    }
                 }
 
-                System.out.print("Midterm: ");
-                try {
-                    gradeData[r][1] = sc.nextDouble();
-                } catch (InputMismatchException e) {
-                    System.out.println("Invalid input. Numbers from 1 to 100 only.");
-                    sc.nextLine(); // clear bad input
+                // Prompt user to enter midterm grade
+                double m = 0;
+                while (true) {
+                    System.out.print("Midterm: ");
+                    try {
+                        m = sc.nextDouble();
+                        if (m >= 1 && m <= 100) {
+                            gradeData[count][1] = m;
+                            break; // escape if user entered a valid input
+                        } else
+                            System.out.println("Invalid input. Enter from 1 to 100 only.\n");
+                    } catch (InputMismatchException e) {
+                        System.out.println("Invalid input. Enter from 1 to 100 only.\n");
+                        sc.nextLine();
+                    }
                 }
 
-                System.out.print("Finals: ");
-                try {
-                    gradeData[r][2] = sc.nextDouble();
-                } catch (InputMismatchException e) {
-                    System.out.println("Invalid input. Numbers from 1 to 100 only.");
-                    sc.nextLine(); // clear bad input
+                // Prompt user to enter final grade
+                double f = 0;
+                while (true) {
+                    System.out.print("Final: ");
+                    try {
+                        f = sc.nextDouble();
+                        if (f >= 1 && f <= 100) {
+                            gradeData[count][2] = f;
+                            break; // escape if user entered a valid input
+                        } else
+                            System.out.println("Invalid input. Enter from 1 to 100 only.\n");
+                    } catch (InputMismatchException e) {
+                        System.out.println("Invalid input. Enter from 1 to 100 only.\n");
+                        sc.nextLine();
+                    }
                 }
-
                 System.out.println();
-            } else if (choice == 2) {
-                System.out.println("Good bye... Muwah!");
+
+                count++; // increments number of subjects entered
+            } else if (c == 2) {
+                System.out.println("Good bye... Muwah!\n");
                 break;
-            } else {
-                System.out.println("Invalid choice");
-                r--; // invalid choice won't count
-            }
+            } 
         }
 
         sc.close(); // close scanner
@@ -77,11 +123,7 @@ public class GradesApp {
         // Add csv header row
         sb.append("Subject,Prelim,Midterm,Finals\n");
 
-        for (int r = 0; r < subjectName.length; r++) {
-            if (subjectName[r] == null) {
-                break;
-            }
-
+        for (int r = 0; r < count; r++) {
             sb.append(subjectName[r]);
             for (int c = 0; c < gradeData[r].length; c++) {
                 sb.append(",").append(gradeData[r][c]);
@@ -90,7 +132,7 @@ public class GradesApp {
         }
 
         // For writing file
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter("grade.csv"))) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("grades.csv"))) {
             bw.write(sb.toString());
             bw.flush();
         } catch (IOException e) {
