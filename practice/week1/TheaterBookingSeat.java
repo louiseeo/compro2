3685a2d0728808db59e76765995ef405a986259f
louiseeo <louisenicole.madriaga@lorma.edu>
2026-02-19 19:49:25 +0800

    practice

--- practice/week1/TheaterBookingSeat.java
+++ practice/week1/TheaterBookingSeat.java
@@ -1,8 +1,38 @@
 import java.util.Scanner;
-  public class TheaterBookingSeat{
-// EXTRA ACTIVITY(not included in part 3 instructions)
+public class TheaterBookingSeat {
+    public static void main(String[] args) {
+        // 1. Declare and initialize the 2D array for the theater
+        int[][] theater = new int[5][8]; // 5 rows, 8 columns
+
+        // 2. Book the seat at row 2, column 5
+        theater[1][4] = 1;
+
+        // 3. Book the seat at row 0, column 0
+        theater[0][0] = 1;
+
+        System.out.println("\nTheater Seating Chart (|-| = Available, |x| = Booked):");
+        // 4. Use nested loops to print the seating chart
+        int seatBookCount = 0; // initialize for counting of available seats
+        String seat = " ";
+        for (int i = 0; i < theater.length; i++) {
+            for (int j = 0; j < theater[i].length; j++) {
+                if (theater[i][j] == 1) {
+                    System.out.print("|x| ");
+                    seatBookCount++;
+                } else {
+                    System.out.print("|-| ");
+                }
+            }
+            System.out.println();
+        }
+
+        // 5. Count and print the total number of booked seats
+        System.out.println("The total number of booked seats are: " + seatBookCount);
+
+        // EXTRA ACTIVITY(not included in part 3 instructions)
         Scanner input = new Scanner(System.in);
         String answer;
+
 
         do {
             while (true) {
@@ -71,5 +101,4 @@ import java.util.Scanner;
         System.out.println("Booking finished... Goodbye!");
         input.close(); // Close scanner
     }
-}
 }
