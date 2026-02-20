7248938e5f8bdd4a5415e742e07ac98105ef0145
louiseeo <louisenicole.madriaga@lorma.edu>
2026-02-20 10:31:08 +0000

    upd

--- practice/exam/Student.java
+++ practice/exam/Student.java
@@ -2,10 +2,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Student {
+	// Create properties
 	String name;
 	List<Integer> attendanceMarks;	
 	
 	public Student(){
-		attendanceMarks = new ArrayList<>();
+		attendanceMarks = new ArrayList<>(); // initialize to store attendance marks
 	}
 }
